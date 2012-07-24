0d88deb53b2496f9d8692c893e9c494fb2b4ddb2
it09ak1 <andre.kiesslich@live.de>
2012-07-24 07:54:42 +0200

    The user can speak in the mic and the result is shown on the Atmel board, but not perfect.

--- schaltCode/Klavier/Klavier.c
+++ schaltCode/Klavier/Klavier.c
@@ -1,31 +1,46 @@
 #include <avr/io.h>
 #include <inttypes.h>
 #include <avr/interrupt.h>
+//#define F_CPU 16000000
 #include <util/delay.h>
 #include <math.h>
 
-void updateLED()
-{
-	//PORTB = ~(def << i);
-	PORTB = (uint8_t)(~(0b00000001));
 
+void InitADC()
+{
+	PORTA = 0;
+	// MUX0-4 setzen (Datenhandbuch Seite 219)
+	// ADC1 und ADC 0 (diese haben wir jetzt genommen)
+	//ADMUX |= (1<<REFS0) | (1<<ADLAR) | (1<<MUX3) | (1<<MUX1) | (1<<MUX0);
+	// 10x Verstaerkung
+	//ADMUX |= (1<<REFS0) | (1<<MUX3) | (1<<MUX0);
+	// ohne Verstaerkung
+	ADMUX = (1<<REFS0);
+	// ADC3 und ADC 2
+	//ADMUX |= (1<<REFS0) | (1<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX10);
+	ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0) | ( 0 << ADPS2);
 }
 
-ISR(INT0_vect)		// Routine für INT0
+uint16_t ReadADC()
 {
-	//PORTB = (uint8_t)(~(0b00000001));
-	//showLED = 0b00000100;
-	//i++;
-	updateLED();
-	_delay_ms(2000);
-	GIFR &= ~(1 << INTF0);	// weitere angemeldete Interrupts löschen (Entprellung)
+	ADCSRA |= (1 << ADSC);
+	while (!(ADCSRA & (1 << ADIF)));
+
+	ADCSRA |= (1 << ADIF);
+
+	return(ADC);
 }
 
 int main(void)
 {
 	//uint8_t ledOut = 0b00000000;
 	DDRB = 0xff;
-	DDRD = 0x00;
+	//DDRD = 0x00;
+	DDRA = 0x00;
+
+	InitADC();
+	PORTD = 0;
+	PORTB = (uint8_t)(~(0b00000001));
 
 	// Interrupts programmieren
 	//GICR	|= ( (1<<INT0) | (1<<INT1) | (1<<INT2) );	// INT0, 1, 2 zulassen
@@ -37,14 +52,62 @@ int main(void)
 	//sei();
 	//ledOut = 0b00000000;
 
+	int value = 0;
+
 	while(1)
 	{
+
+		value = ReadADC();
+		value = value - 820;
+		value = abs(value);
+		uint8_t vumeter = 0b00000000;
+		
+		if (value > 50) {
+			vumeter |= 0b00000001;
+		}
+		if (value > 55) {
+			vumeter |= 0b00000010;
+		}
+		if (value > 60) {
+			vumeter |= 0b00000100;
+		}
+		if (value > 65) {
+			vumeter |= 0b00001000;
+		}
+		if (value > 70) {
+			vumeter |= 0b00010000;
+		}
+		if (value > 75) {
+			vumeter |= 0b00100000;
+		}
+		if (value > 85) {
+			vumeter |= 0b01000000;
+		}
+		if (value > 100) {
+			vumeter |= 0b10000000;
+		}
+		
+		//value = ReadADC();
+		//value -=850;
+		//value = abs(value);
+		//PORTB = (uint8_t)(~(value));
+		//PORTB = (uint8_t)(~(ReadADC()));
+		PORTB = (uint8_t)(~(vumeter));
+		
+		_delay_ms(100);
+
+		//if (ReadADC() > 1000)
+		//{
+			//PORTB = (PORTB << 1);	
+		//	_delay_ms(2000);
+		//}
 		//if (PIND)
 		//{
 			//PORTB = PIND;
 
 			//selectedPIN = PIND;
 
+		/*
 		if (PIND == 0b11111110)
 		{
 			PORTB = (uint8_t)(~(0b00000001));
@@ -81,6 +144,7 @@ int main(void)
 		{
 			PORTB = (uint8_t)(~(0b00000000));
 		}
+		*/
 		
 
 		//}
